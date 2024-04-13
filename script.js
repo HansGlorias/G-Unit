89c73b4e81b6cba7385d58c97b2ed7befc6fa996
Andrew <92540321+HansGlorias@users.noreply.github.com>
2024-04-13 10:13:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,10 @@
 const audio = document.getElementById("audio");
 const gUnitButton = document.getElementById("gUnitButton")
 
-
-
-
 audio.src = "50 Cent - Disco Inferno.mp3";
 audio.volume = 0.05;
 
-
 gUnitButton.onclick = play
-
-console.log(audio)
 
 function mute(){
 
