2e60b75ff8d6a6b8a1ad31c980a0f9e3cc315eea
Andrew <92540321+HansGlorias@users.noreply.github.com>
2024-04-13 09:57:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+const audio = document.getElementById("audio");
+const gUnitButton = document.getElementById("gUnitButton")
+
+
+
+
+audio.src = "50 Cent - Disco Inferno.mp3";
+audio.volume = 0.05;
+
+
+gUnitButton.onclick = play
+
+console.log(audio)
+
+function mute(){
+
+    audio.muted = true;
+
+}
+
+function play(){
+
+    console.log("playing...")
+    
+    audio.play();
+}
+
